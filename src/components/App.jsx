0e28bbcc69ba19382f064b36cfb50f9d1a54c576
figatix <60873339+figatix@users.noreply.github.com>
2023-02-16 21:34:12 +0200

    ///

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+import Profile from "./Profile";
+import userData from "../dataSrc/user.json";
+
+// const user = `{
+//   "username": "Jacques Gluke",
+//   "tag": "jgluke",
+//   "location": "Ocho Rios, Jamaica",
+//   "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+//   "stats": {
+//     "followers": 5603,
+//     "views": 4827,
+//     "likes": 1308
+//   }
+// }`
+
+const { username, tag, location, avatar, stats } = userData;
+
+// const username = "Jacques Gluke";
+// const tag = "jgluke";
+//   const location = "Ocho Rios, Jamaica";
+//   const avatar = "https://cdn-icons-png.flaticon.com/512/2922/2922506.png";
+//   const stats = {
+//     "followers": 5603,
+//     "views": 4827,
+//     "likes": 1308
+//   }
+
 export const App = () => {
+  // console.log('123123',userParse);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    <Profile
+      username={username}
+      tag={tag}
+      location={location}
+      avatar={avatar}
+      stats={stats}
+    />
+  )
 };
---
+++ src/components/Profile.jsx
@@ -0,0 +1,81 @@
+
+
+export default function Profile(props) {
+  
+  const { username, tag, location, avatar, stats } = props;
+
+  return (
+    <div className="profile">
+      <div className="description">
+        <img
+          src={avatar}
+          alt="User avatar"
+          className="avatar"
+        />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+
+/*
+
+Опис компонента <Profile>
+Компонент повинен приймати кілька пропсів з інформацією про користувача:
+
+username — ім'я користувача
+tag — тег в соціальній мережі без @
+location — місто і країна
+avatar — посилання на зображення
+stats — об'єкт з інформацією про активності
+
+
+===============================
+
+<div class="profile">
+  <div class="description">
+    <img
+      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+      alt="User avatar"
+      class="avatar"
+    />
+    <p class="name">Petra Marica</p>
+    <p class="tag">@pmarica</p>
+    <p class="location">Salvador, Brasil</p>
+  </div>
+
+  <ul class="stats">
+    <li>
+      <span class="label">Followers</span>
+      <span class="quantity">1000</span>
+    </li>
+    <li>
+      <span class="label">Views</span>
+      <span class="quantity">2000</span>
+    </li>
+    <li>
+      <span class="label">Likes</span>
+      <span class="quantity">3000</span>
+    </li>
+  </ul>
+</div>
+
+*/
